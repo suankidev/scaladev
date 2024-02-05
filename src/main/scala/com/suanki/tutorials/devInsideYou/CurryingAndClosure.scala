794c206659243adaa2e35c61eff3b3e5e033b8c4
suankidev <suankisingh@outlook.com>
2024-02-06 01:03:42 +0530

    currying

--- src/main/scala/com/suanki/tutorials/devInsideYou/CurryingAndClosure.scala
+++ src/main/scala/com/suanki/tutorials/devInsideYou/CurryingAndClosure.scala
@@ -4,8 +4,22 @@ object CurryingAndClosure extends App {
 
   println("=" * 100)
 
+  // function literal are closure
+  // x is free variable
+  // y is bound variable
+  // openTerm addToX method is openTerm b/c its using free variables
+  var x = 1337
+
+  def addToX(y: Int): Int =
+    x + y // function depends on outside variable is called closure
+  def addToX1: Int => Int = y => x + y
+
+  // calling
+  println(addToX(y = 10))
+  println(addToX1(10))
+
   // var freeVariable:Int = 1337
-  //
+  // produces another function as result call function currying
   def closedTerm(freeVariable: Int): Int => Int =
     boundVariable => freeVariable + boundVariable // anonymous function
 
@@ -19,33 +33,10 @@ object CurryingAndClosure extends App {
   println(UncurriedclosedTermOne(freeVariable = 7, boundVariable = 8))
   println(closedTermOne(freeVariable = 7)(8))
 
-  //      //createTableDataFrame(buildQuery,dffromQuery)
-  //  //            ==> buildQuery(buildSelect,buldWhere)
-  //  //                     ==> buildSelect
-  //  //                          ==>buildeWhere(fetchPartcolumn)
-  //
-  //  def buildSelect(tableName:String)=s"select * from $tableName"
-  //
-  //  def buildWhere(tableName:String, spark:SparkSession,f:(String,SparkSession) => List[String]) = {
-  //    var where = "1 = 1 AND "
-  //    f(tableName,spark) foreach(
-  //      _ match {
-  //        case "cobDate" => where = s"AND cobDate='2023-12-30'"
-  //      }
-  //    )
-  //
-  //  }
-  //
-  //  def buildQuery(tableName:String, spark:SparkSession, f:(String,SparkSession) => DataFrame)=
-  //    s"select * from $tableName where ${f(tableName,spark)}"
-  //
-  //  def createDataFrames(tableName:String,spark:SparkSession, f:(String,SparkSession) => String, g:(String,SparkSession) => DataFrame)={
-  //    var SqlQuery = f(tableName,spark)
-  //
-  //
-  //  }
-  //
-  // createDataFrames(tableName,spark,dfType)
+  // more maintainable
+  val partialComputation = closedTerm(freeVariable = 1337)
+
+  println(partialComputation(20))
 
   def code(args: Array[String]): Unit = {
 
@@ -70,6 +61,60 @@ object CurryingAndClosure extends App {
     def g(x: Int): String = s"${Console.MAGENTA} x ${Console.RESET}"
 
     f(g)
+
+  }
+
+  // practical example
+  def code2(): Unit = {
+
+    // replace spark/dataframe  with string type not to trigger spark session
+
+    //      //createTableDataFrame(buildQuery,dffromQuery)
+    //  //            ==> buildQuery(buildSelect,buldWhere)
+    //  //                     ==> buildSelect
+    //  //                          ==>buildeWhere(fetchPartcolumn)
+    //
+    def buildSelect(tableName: String) = s"select * from $tableName"
+
+    def buildWhere(
+        tableName: String,
+        spark: String,
+        f: (String) => List[String]
+    ) = {
+      var where = "1 = 1 AND "
+      f(tableName) foreach (
+        _ match {
+          case "cobDate" => where = s"AND cobDate='2023-12-30'"
+        }
+      )
+
+    }
+
+    def getPartition(tableName: String) = List("date", "id")
+
+    def buildQuery(
+        tableName: String,
+        spark: String,
+        fun: String => List[String]
+    ) =
+      s"${buildSelect(tableName)} where ${buildWhere(tableName, spark, fun)}"
+
+    def createDataFrames(
+        tableName: String,
+        spark: String,
+        f: (String) => String,
+        tableType: String
+    ): String = {
+
+      getDf(buildQuery(tableName, spark, getPartition))
+
+    }
+
+    def getDf(query: String): String = query
+
+    val stgDF =
+      createDataFrames("table_stg", "spark", getDf, "tableTypeStg")
+    val finalDF = createDataFrames("table", "spark", getDf, "tableTypeStg")
 
   }
 
