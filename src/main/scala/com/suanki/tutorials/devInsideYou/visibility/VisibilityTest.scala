f3fc972a9fca6a5d99a0b59c58fac386e5f956da
suankidev <suankisingh@outlook.com>
2024-02-16 16:32:30 +0530

    trait superpower

--- src/main/scala/com/suanki/tutorials/devInsideYou/visibility/VisibilityTest.scala
+++ src/main/scala/com/suanki/tutorials/devInsideYou/visibility/VisibilityTest.scala
@@ -1,5 +1,15 @@
 package com.suanki.tutorials.devInsideYou.visibility
-// private[this]          //only accessible to current object
+
+/** private[this] //only accessible to current object private //only instance of current object protected[this] //only
+  * current instatnce and the subclass instance protexte //any instance of the current class and any instatnce of any
+  * sub class
+  *
+  * final on a class mean that it can't be extended final //on trait not allowed final //on any member means can't ve
+  * overwrittend
+  *
+  * abstract can't be instantiated abstrace //on any member is not allowed
+  */
+
 // example
 class A {
 
